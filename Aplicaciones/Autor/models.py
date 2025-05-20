3a39a77c7f8c3c4321cfbf6301ccefa24ee58c49
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-19 22:45:26 -0500

    Se agrego el modelo

--- Aplicaciones/Autor/models.py
+++ Aplicaciones/Autor/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 class Autor(models.Model):
     id_autor = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)

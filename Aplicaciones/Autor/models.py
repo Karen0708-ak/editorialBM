0f0b881af7157c47091cce11bcf3262fb009e26d
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-19 22:21:06 -0500

    Se agregaron los modelos de autor y libreria

--- Aplicaciones/Autor/models.py
+++ Aplicaciones/Autor/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    id_autor = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=100)
--- Aplicaciones/Libreria/models.py
+++ Aplicaciones/Libreria/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Libreria(models.Model):
+    id_libreria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200, blank=True)

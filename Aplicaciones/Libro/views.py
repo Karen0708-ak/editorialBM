e161554207512400bbe11a4d4c016600e4477c1e
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-19 23:52:04 -0500

    Se agrego la vista para libro

--- Aplicaciones/Libro/views.py
+++ Aplicaciones/Libro/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+#importando el modelo plato
+from .models import Libro
+from django.contrib import messages
+#esta función es para renderizar el listado de Plato
+def inicio(request):
+    listadoLibro=Libro.objects.all()
+    return render(request,"iniciobo.html",{'libro':listadoLibro})

f818b3a1fe4d0d3db6a2a23994129e5461662152
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-20 03:35:36 -0500

    Se agrego las vitas para Libreria

--- Aplicaciones/Libreria/views.py
+++ Aplicaciones/Libreria/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Libreria
+from django.contrib import messages
 
-# Create your views here.
+def inicio(request):
+    listadoLibreria=Libreria.objects.all()
+    return render(request,"inicioli.html",{'libreria':listadoLibreria})
+
+def nuevaLibreria(request):
+    return render(request,"nuevaLibreria.html")
+#Almacenando los datos de cargo en la Bdd
+def guardarLibreria(request):
+    nombre = request.POST["nombre"]
+    direccion= request.POST["direccion"]
+    nuevaLibreria=Libreria.objects.create(
+            nombre=nombre,
+            direccion=direccion,
+        )
+    #mensaje de confirmacion
+    messages.success(request,"Libreria guardado exitosamente")
+    return redirect('inicioli')
+def eliminarLibreria(request,id_libreria):
+    libreriaEliminar = Libreria.objects.get(id_libreria=id_libreria)
+    libreriaEliminar.delete()
+    messages.success(request,"Libreria ELIMINADA exitosamente")
+    return redirect('inicioli')
+
+#editar
+def editarLibreria(request,id_libreria):
+    libreriaEditar=Libreria.objects.get(id_libreria=id_libreria)
+    return render(request,"editarLibreria.html",{'libreriaEditar':libreriaEditar})
+
+def procesarEdicionLibreria(request):
+    id_libreria=request.POST["id_libreria"]
+    nombre = request.POST["nombre"]
+    direccion = request.POST["direccion"]
+    libreria=Libreria.objects.get(id_autor=id_libreria)
+    libreria.nombre=nombre
+    libreria.direccion=direccion
+    libreria.save()
+    #mensaje de confirmacion
+    messages.success(request,"Libreria ACTUALIZADA exitosamente")
+    return redirect('inicioli')
